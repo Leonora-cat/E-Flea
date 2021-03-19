bf21b93a97f3541f877c4ef29c0a3d0f87fab622
Blurryblurryfaces <48159142+d4f2e8@users.noreply.github.com>
2021-03-19 15:50:05 +0800

    Update userTable.sql

--- userTable.sql
+++ userTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user` (
 	`id` BIGINT NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-	`pssword` VARCHAR(255) DEFAULT NULL,
+	`password` VARCHAR(255) DEFAULT NULL,
 	`date_of_birth` date DEFAULT NULL,
 	`sex` boolean DEFAULT NULL,
 	PRIMARY KEY (`id`)
