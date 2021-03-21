200ebc678bbbc798b14c2c3c220f934793356ae9
Blurryblurryfaces <48159142+d4f2e8@users.noreply.github.com>
2021-03-21 11:02:42 +0800

    Add files via upload
    fix a mistake

--- eflea/src/main/java/cpt202/bughunter/eflea/mapper/UserMapper.java
+++ eflea/src/main/java/cpt202/bughunter/eflea/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package cpt202.bughunter.eflea.mapper;
 import cpt202.bughunter.eflea.domain.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.sql.Date;
@@ -10,8 +11,8 @@ import java.sql.Date;
 @Mapper
 public interface UserMapper {
     @Select("SELECT * FROM user WHERE username=#{username} AND password=#{password}")
-    User getUserByNameAndPwd(String username, String password);
+    User getUserByNameAndPwd(@Param("username") String username, @Param("password") String password);
 
     @Insert("INSERT INTO user(username,password,data_of_birth,sex) VALUES (#{username},#{password},#{dateOfBirth},#{sex});")
-    void insertUser(String username, String password, Date dateOfBirth, Boolean sex);
+    void insertUser(@Param("username") String username, @Param("password") String password, @Param("dateOfBirth") Date dateOfBirth, @Param("sex") Boolean sex);
 }
