62f9787830d76fb64c53906c9b55b9b83665f2c8
Blurryblurryfaces <48159142+d4f2e8@users.noreply.github.com>
2021-03-21 16:59:56 +0800

    Add files via upload
    1. Add CipherUtil class
    2. Password now can be encrypted before storing into database.

--- eflea/src/main/java/cpt202/bughunter/eflea/web/UserController.java
+++ eflea/src/main/java/cpt202/bughunter/eflea/web/UserController.java
@@ -2,6 +2,7 @@ package cpt202.bughunter.eflea.web;
 
 import cpt202.bughunter.eflea.domain.User;
 import cpt202.bughunter.eflea.service.UserService;
+import cpt202.bughunter.eflea.util.CipherUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +14,20 @@ import java.util.Map;
 
 @RestController
 public class UserController {
+    private boolean cipherOn = true;
     @Autowired
     UserService userService;
 
     @PostMapping("/user")
     @ApiOperation(value = "login", notes = "receive username and password, if the user exists, return all the info")
-    public Map<String, Object> toLogin(@RequestBody User user){
+    public Map<String, Object> toLogin(@RequestBody User user) throws Exception {
         Map<String,Object> userMap = new HashMap<>();
-        User realUser = userService.toLogin(user.getUsername(),user.getPassword());
+        User realUser;
+        if(cipherOn) {
+            realUser = userService.toLogin(user.getUsername(), CipherUtil.encrypt(user.getPassword()));
+        }else{
+            realUser = userService.toLogin(user.getUsername(), user.getPassword());
+        }
         if(realUser!=null){
             userMap.put("status","success");
             userMap.put("userInfo",realUser);
