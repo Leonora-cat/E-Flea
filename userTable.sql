a45f324f264bd6074bcb8f2907d4def9f302062b
Blurryblurryfaces <48159142+d4f2e8@users.noreply.github.com>
2021-03-22 15:31:06 +0800

    Add files via upload
    Edit attributes

--- userTable.sql
+++ userTable.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `user` (
-	`uId` BIGINT NOT NULL AUTO_INCREMENT,
+	`userId` BIGINT NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR ( 255 ) COLLATE utf8mb4_general_ci DEFAULT NULL,
 	`password` VARCHAR ( 255 ) DEFAULT NULL,
 	`email` VARCHAR ( 255 ) DEFAULT NULL,
+	`major` VARCHAR (255) DEFAULT NULL,
 	`dateOfBirth` date DEFAULT NULL,
 	`sex` boolean DEFAULT NULL,
-PRIMARY KEY ( `uId` ) 
+PRIMARY KEY ( `userId` ) 
 ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
