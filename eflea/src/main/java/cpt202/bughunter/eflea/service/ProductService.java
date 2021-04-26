b11691d3895227f783775f5d75f7a2d4f90ec8ff
OOOG <46337960+OOOG1999@users.noreply.github.com>
2021-04-26 15:15:22 +0800

    Add files via upload

---
+++ eflea/src/main/java/cpt202/bughunter/eflea/service/ProductService.java
@@ -0,0 +1,31 @@
+package cpt202.bughunter.eflea.service;
+
+import com.github.pagehelper.PageInfo;
+import cpt202.bughunter.eflea.domain.Product;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+public interface ProductService {
+    List<Product> allProduct();
+    List<Product> displayAll(String tag);
+    List<Product> findProductByName(String name);
+    List<Product> findByTag(@Param("tag") String tag);
+    Product findProductByPid(Long id);
+    List<Product> searchAll(String key);
+    void insert(Long id, String name, Double price, String tag, String lifeTime, String description, String uid, Boolean state, Date time);
+    void delete(Long id);
+    void update(Long id, String name, Double price, String tag1, String lifeTime, String description);
+    void insertImage(byte[] image1, byte[] image2, byte[] image3, Long id);
+    void updateState(Boolean state, Long pid);
+    Product findProductByPName(String name);
+    void publishTime(Date time, Long pid);
+    List<Product> search(String key, String name);
+    List<Product> findAllProductByPageF(int pageNum,int pageSize);
+    PageInfo<Product> findAllProductByPageS(int pageNum, int pageSize);
+
+
+}
