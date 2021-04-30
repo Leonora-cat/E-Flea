7df64d3f8b763ed71346ef5897b6103f3c0e8121
OOOG <46337960+OOOG1999@users.noreply.github.com>
2021-04-30 17:04:20 +0800

    Completed demo
    Release

---
+++ eflea/src/main/java/cpt202/bughunter/eflea/mapper/MessageMapper.java
@@ -0,0 +1,22 @@
+package cpt202.bughunter.eflea.mapper;
+
+import cpt202.bughunter.eflea.domain.Message;
+import org.apache.ibatis.annotations.*;
+
+import java.sql.Date;
+import java.util.List;
+
+@Mapper
+public interface MessageMapper {
+    @Select("SELECT COUNT(*) FROM message WHERE receiver=#{username}")
+    int getMessageNum(@Param("username") String username);
+
+    @Select("SELECT DISTINCT sender FROM message WHERE receiver=#{username} OR sender=#{username}")
+    List<String> getContactList(@Param("username") String username);
+
+    @Select("SELECT content,sender,receiver,sendTime FROM message where (sender=#{sender} AND receiver=#{receiver}) OR (sender=#{receiver} AND receiver=#{sender}) ORDER BY sendTime ASC")
+    List<Message> getContactDetail(@Param("sender") String sender, @Param("receiver") String receiver);
+
+    @Insert("INSERT INTO message(sender,receiver,content) VALUES (#{sender},#{receiver},#{content})")
+    void addNewContent(@Param("sender") String sender, @Param("receiver") String receiver,@Param("content") String content);
+}
---
+++ eflea/src/main/java/cpt202/bughunter/eflea/service/MessageService.java
@@ -0,0 +1,17 @@
+package cpt202.bughunter.eflea.service;
+
+import cpt202.bughunter.eflea.domain.Message;
+import org.apache.ibatis.annotations.Param;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface MessageService {
+    int getMessageNum(String username);
+
+    List<String> getContactList(String username);
+
+    List<Message> getContactDetail(String user1, String user2);
+
+    void addNewContent(String sender, String receiver, String content);
+}
